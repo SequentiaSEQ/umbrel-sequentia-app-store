462b156f567561669eecf2d008497841000d8f82
Mihailo Milenkovic <mihailo.milenkovic84@gmail.com>
2025-05-07 19:54:17 +0200

    Update

--- sequentia-testnet/exports.sh
+++ sequentia-testnet/exports.sh
@@ -11,9 +11,11 @@ export APP_SEQUENTIA_ELECTRS_WEB_PORT=3001
 
 app_sequentia_rpc_hidden_service_file="${EXPORTS_TOR_DATA_DIR}/app-${EXPORTS_APP_ID}-rpc/hostname"
 app_sequentia_p2p_hidden_service_file="${EXPORTS_TOR_DATA_DIR}/app-${EXPORTS_APP_ID}-p2p/hostname"
+app_sequentia_electrs_hidden_service_file="${EXPORTS_TOR_DATA_DIR}/app-${EXPORTS_APP_ID}-electrs/hostname"
 
 export APP_SEQUENTIA_RPC_HIDDEN_SERVICE="$(cat "${app_sequentia_rpc_hidden_service_file}" 2>/dev/null || echo "notyetset.onion")"
 export APP_SEQUENTIA_P2P_HIDDEN_SERVICE="$(cat "${app_sequentia_p2p_hidden_service_file}" 2>/dev/null || echo "notyetset.onion")"
+export APP_SEQUENTIA_ELECTRS_HIDDEN_SERVICE="$(cat "${app_sequentia_electrs_hidden_service_file}" 2>/dev/null || echo "notyetset.onion")"
 
 export APP_SEQUENTIA_RPC_USER="umbrel"
 
